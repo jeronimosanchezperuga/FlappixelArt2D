ca18f51b38eed744048d71b7861dfc77af6afb81
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2022-10-24 15:58:42 -0300

    Contador de segundos en pantalla

---
+++ Assets/TimeCounter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TimeCounter : MonoBehaviour
+{
+    public TextMeshProUGUI timeText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int seconds = Mathf.FloorToInt(Time.time);
+        timeText.text = seconds.ToString();
+    }
+}
---
+++ Assets/TimeLapseCounter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TimeLapseCounter : MonoBehaviour
+{
+    public TextMeshProUGUI txtLapse;
+    float elapsedTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            elapsedTime = 0;
+        }
+
+        elapsedTime = elapsedTime + Time.deltaTime;
+        txtLapse.text = Mathf.Floor(elapsedTime).ToString();
+    }
+}
